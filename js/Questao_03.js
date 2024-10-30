04a3273756273c94408f4db0499e78e972d41904
JPBatelli <160743082+JPBatelli@users.noreply.github.com>
2024-10-30 09:21:51 -0300

    primeira alteração

---
+++ js/Questao_03.js
@@ -0,0 +1,14 @@
+var nomeAluno = "João Pedro de Mirana Batelli";
+var nota_01 = 10;
+var nota_02 = 8,8;
+var nota_01_peso = nota_01 * 0.6;
+var nota_02_peso = nota_02 * 0.4;
+var nota_final = nota_01_peso + nota_02_peso;
+
+alert("Nome do aluno: " + nomeAluno + "\n" + 
+	  "Nota 1: " + nota_01 + "\n" + 
+	  "Nota 2: " + nota_02 + "\n");
+
+alert('Nome do aluno: ' + nomeAluno + "\n" + 
+	  "Nota final: " + nota_final+ "\n" + 'coloquei as minhas notas');
+	  "Nota final: " + nota_final);
